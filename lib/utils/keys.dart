9fc99c47b60c57f718e2a3024445fcb4284de590
VietHoangTran <viethoangtran3110@gmail.com>
2024-05-22 08:51:30 +0700

    init

---
+++ lib/utils/keys.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// String googleApiKey = dotenv.env['GOOGLE_API_KEY']!;
+String googleApiKey = 'AIzaSyDZF_oNL8olqMqAbgcjl6HU1vj2eEjx1Hw';
+String backendUrl = dotenv.env['BACKEND_URL']!;
+String backendApiUrl = "${dotenv.env['BACKEND_URL']}";
